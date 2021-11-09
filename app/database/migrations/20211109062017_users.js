b9d2f6d640b2ffcb874b43c7f1cbf346fc702d93
Thomas Li <thomasligreat11@gmail.com>
2021-11-08 22:33:12 -0800

    feat: made boiler plate and database migration files

---
+++ app/database/migrations/20211109062017_users.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('users', (tbl) => {
+      tbl.increments('id').unique().notNullable();
+      tbl.string('email').notNullable();
+      tbl.string('first_name').notNullable();
+      tbl.string('last_name').notNullable();
+      tbl.text('password');
+  });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('users');
+};
---
+++ app/database/migrations/20211109062709_saved_recipes.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('saved_recipes', (tbl) => {
+        tbl.increments('id').unique().notNullable();
+        tbl.integer('uid').notNullable();
+        tbl.integer('sid');
+        tbl.integer('rid');
+        tbl.boolean('is_creator').notNullable();
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('saved_recipes');
+};
