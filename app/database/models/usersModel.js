62c703e897408d19f4c33ac15b27a7b87b120f9e
Thomas Li <thomasligreat11@gmail.com>
2021-11-09 20:37:11 -0800

    feat: added functions for insert/get from users table

---
+++ app/database/models/usersModel.js
@@ -0,0 +1,31 @@
+const db = require('../dbConfig');
+
+/**
+ * get user by id
+ * @param id
+ * @returns array []
+ */
+async function getById(id){
+    const rows = await db('users')
+        .where({id})
+        .select('*');
+    return rows;
+}
+
+/**
+ * insert user into users table
+ * @param user
+ * @returns {Promise<{status: string}>}
+ */
+async function insert(user){
+    try {
+        await db('users')
+            .insert(user);
+        return {status: 'success'};
+    } catch(err){
+        console.error(err);
+        return {status: 'failed'};
+    }
+}
+
+module.exports={getById, insert};
