e2e1714209c94f8350b0f7cac60372a8d0ca6365
s8sun <s8sun@ucsd.edu>
2021-11-14 21:39:09 -0800

    Create bad_indent.js
    For cicd testing

---
+++ app/cicd_testing/bad_indent.js
@@ -0,0 +1,14 @@
+
+let myDictionary = {
+	'value 1': "string",
+"value 2": 2,
+	"value 3": ["array value 1", "array value 2"]
+};
+
+// Access the dictionary using its keys
+var value1 = myDictionary["value1"]; // Type: String
+
+
+
+var value2 = myDictionary["value2"]; // Type: Int
+var value3 = myDictionary["value3"]; // Type: Array
