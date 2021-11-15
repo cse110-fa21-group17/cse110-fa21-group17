798044c7708b84c5b0a7440e85866e84adadeafd
s8sun <s8sun@ucsd.edu>
2021-11-14 22:18:29 -0800

    Create bad_style.js

---
+++ cicd_testing/bad_style.js
@@ -0,0 +1,7 @@
+var        colors = ["red",     "blue",   'green'];
+
+//you can loop through an array like this:
+for (var i = 0; i < colors.length    ; i++) {
+console.log(colors[i]);
+    let unused = 99;
+}
