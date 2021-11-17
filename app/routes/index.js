389f247f610378027e7f05d13511ca394acd80be
Thomas Li <30820907+ThomasLiARDJAVA@users.noreply.github.com>
2021-11-17 01:06:58 +0000

    Apply suggestions from code review
    Co-authored-by: rcherukuru2000 <78569296+rcherukuru2000@users.noreply.github.com>

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require("bcrypt-nodejs");
 
 const usersModel = require('../database/models/usersModel');
 
