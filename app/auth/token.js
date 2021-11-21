01621a044ea0746a34b61574a232953bd91eb767
Thomas Li <30820907+ThomasLiARDJAVA@users.noreply.github.com>
2021-11-21 01:33:30 +0000

    added token utility (#32)
    * added token utility
    
    * Apply suggestions from code review
    
    Co-authored-by: rcherukuru2000 <78569296+rcherukuru2000@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: rcherukuru2000 <78569296+rcherukuru2000@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: rcherukuru2000 <78569296+rcherukuru2000@users.noreply.github.com>
    
    * added middleware
    
    Co-authored-by: rcherukuru2000 <78569296+rcherukuru2000@users.noreply.github.com>

---
+++ app/auth/token.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+/**
+ * validates the token, and return the user json.
+ * @param token
+ * @returns {Promise<user json>}
+ */
+async function validateToken(token){
+  return jwt.verify(token, process.env.SECRETKEY, async function(err, authData) {
+        if (err) {
+      return null;
+    } else {
+      return authData;
+    }
+  });
+}
+
+/**
+ * generate a token with the given user.
+ * @param userDataPacket
+ * @return {Promise<token|*>}
+ */
+async function generateToken(user){
+  return jwt.sign({user}, process.env.SECRETKEY);
+}
+
+module.exports = {
+  validateToken,
+  generateToken
+};
