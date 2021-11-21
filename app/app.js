ee2bd7b8e6aa004f87b2e0e6dd7b8aef411b798f
WilliamLi0201 <60164781+WilliamLi0201@users.noreply.github.com>
2021-11-21 11:35:59 -0800

    new recipe backend connection (#36)
    * made dashboard route
    
    * new_recipe
    
    * new_recipe_back
    
    * added insertAsCreator
    
    * added exports
    
    * added middleware
    
    * new_recipe_database_connect
    
    * changed middleware redirect
    
    * moved new_recipe routes
    
    * moved import
    
    * added pages
    
    * added recipe success page
    
    Co-authored-by: Thomas Li <thomasligreat11@gmail.com>
    Co-authored-by: Thomas Li <30820907+ThomasLiARDJAVA@users.noreply.github.com>

--- app/app.js
+++ app/app.js
@@ -5,7 +5,10 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const verifyCookieToken = require('./middlewares/verifyCookieToken');
+
 const indexRouter = require('./routes/index');
+const dashboardRouter = require('./routes/dashboard');
 const usersRouter = require('./routes/users');
 
 const app = express();
@@ -21,6 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/dashboard', verifyCookieToken, dashboardRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
