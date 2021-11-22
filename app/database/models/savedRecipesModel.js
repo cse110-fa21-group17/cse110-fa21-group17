44dce6c353b9c94edec60ba00276abf604bec197
Thomas Li <30820907+ThomasLiARDJAVA@users.noreply.github.com>
2021-11-22 23:53:16 +0000

    finished update and delete of CRUD (#44)

--- app/database/models/savedRecipesModel.js
+++ app/database/models/savedRecipesModel.js
@@ -24,5 +24,34 @@ async function getByUidAndNullRid(uid){
     return data;
 }
 
-module.exports={getByUid, getByUidAndNullRid};
+/**
+ * remove by user id and recipe id
+ * @param uid
+ * @param rid
+ * @returns {Promise<void>}
+ */
+async function removeByUidAndRid(uid, rid){
+    try {
+        await db('saved_recipes')
+            .del()
+            .where({uid, rid});
+    } catch (err){
+        console.error(err);
+    }
+}
+
+/**
+ * get saved recipes by uid and rid
+ * @param uid
+ * @param rid
+ * @returns {Promise<void>}
+ */
+async function getByUidAndRid(uid, rid){
+    const data = await db('saved_recipes')
+        .select('*')
+        .where({uid, rid});
+    return data;
+}
+
+module.exports={getByUid, getByUidAndNullRid, removeByUidAndRid, getByUidAndRid};
 
---
+++ app/middlewares/validators/isOwnerOfRecipe.js
@@ -0,0 +1,27 @@
+const savedRecipesModel = require('../../database/models/savedRecipesModel');
+/**
+ * verifies cookie token for each parking spot, and returns info back from cookie to req.spotInfo.
+ * @param req
+ * @param res
+ * @param next
+ * @returns {Promise<void>}
+ */
+async function isOwnerOfRecipe(req, res, next) {
+    try {
+        const uid = req.user.id;
+        const rid = req.params.id;
+        const ownership = await savedRecipesModel.getByUidAndRid(uid, rid);
+        if (ownership.length > 0 && ownership[0].is_creator) {
+            next();
+        } else {
+            return res.status(401)
+                .json({status: 'forbidden', message: 'Your are not the owner of this recipe!'});
+        }
+    } catch (err){
+        console.error(err);
+        return res.status(401)
+            .json({status: 'forbidden', message: 'Your are not the owner of this recipe!'});
+    }
+}
+
+module.exports=isOwnerOfRecipe;
