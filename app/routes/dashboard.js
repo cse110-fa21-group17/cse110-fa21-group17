8be81ccd369532fed88ccb7b9b1b671f0b5a5d25
Thomas Li <thomasligreat11@gmail.com>
2021-11-22 12:17:31 -0800

    removed recipes from spoonacular on cookbook

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -2,21 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const recipesModel = require('../database/models/recipesModel');
-const savedRecipesModel = require('../database/models/savedRecipesModel');
 
 router.get('/', async function(req, res, next){
    try {
        const uid = req.user.id;
-       const savedSpoonRecipes = await savedRecipesModel.getByUidAndNullRid(uid);
        const recipes = await recipesModel.getByUid(uid);
-       const ids = await Promise.all(savedSpoonRecipes.map(recipe => {
-           return recipe.sid;
-       }));
-       if(ids.length !== 0){
-           const bulkResponse = await axios.get(`https://api.spoonacular.com/recipes/informationBulk?ids=${ids.join(',')}&apiKey=${process.env.SPOON_API}`);
-           const spoonRecipes = bulkResponse.data;
-           recipes.concat(spoonRecipes);
-       }
        return res.render('pages/mycookbook', {title: 'My Cook Book', recipes});
    } catch (err){
        console.error(err);
