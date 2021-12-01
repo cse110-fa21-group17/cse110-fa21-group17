3b85b5a9f6d25e1b5b3297859ba2682101f2e51e
cbj99 <bac001@ucsd.edu>
2021-12-01 15:30:09 -0800

    Co-authored-by: Jackie Kwok <jkwok626@users.noreply.github.com> Co-authored-by: Eric Peng <3ricpeng@users.noreply.github.com> Co-authored-by: XinchenZhang <XinchenZhang@users.noreply.github.com>

---
+++ app/_tests_/project.test.js
@@ -0,0 +1,49 @@
+describe('Basic user flow for Hot Dawg', () => {
+    // First, visit our app
+
+    jest.setTimeout(50000);
+    beforeAll(async () => {
+      //await page.goto('http://localhost:3000/');
+      await page.goto('http://hotstage-env.eba-impqun6r.us-west-1.elasticbeanstalk.com/');
+    });
+  
+    // Next, check to make sure that all 8 recipe cards have loaded
+    it('Initial landing Page - Check for 8 recipes', async () => {
+      console.log('Checking for recipes on landing page...');
+      // Query select all of the <div.recipeCard> elements and return the length of that array
+      const numRecipes = await page.$$eval('div.recipeCard', (recipeList) => {
+        return recipeList.length;
+      });
+      // Expect there that array from earlier to be of length 8, meaning 8 <div.recipeCard> elements where found
+      expect(numRecipes).toBe(8);
+    });
+  
+    // Check to make sure that all 8 recipe cards have data in them
+    it('Make sure recipe cards are populated', async () => {
+      console.log('Checking to make sure recipe cards are populated...');
+      // Start as true, if any don't have data, swap to false
+      let allArePopulated = true;
+      let data, plainValue;
+      // Query select all of the recipe cards
+      const recipeList = await page.$$('div.recipeCard');
+      
+      for (let i = 0; i < recipeList.length; i++) {
+        console.log(`Checking recipe ` + (i + 1) + `/${recipeList.length}`);
+
+        
+        var title = await recipeList[i].$('cardText');
+        // Make sure we have the text and image
+        //if (title) { allArePopulated = false; }
+        //if (recipeList[i].cardText > p.recipeTitle) { allArePopulated = false; }
+        if (recipeList[i].cardText.card-tags) { allArePopulated = false; }
+        if (recipeList[i].cardText.card-details) { allArePopulated = false; }
+        if (recipeList[i].cardImage.src) { allArePopulated = false; }
+        
+        // Expect allArePopulated to still be true
+        expect(allArePopulated).toBe(true);
+      }
+  
+    }, 10000);
+  
+    
+  });
