ee9aefa6d3ef89e1aad874041fba233979598b9d
Benjamin Kuo <bkuo@ucsd.edu>
2021-11-30 23:36:53 -0800

    fixed btn change from Added to Add

--- app/public/javascripts/add_button.js
+++ app/public/javascripts/add_button.js
@@ -3,10 +3,12 @@ let btn = document.getElementById('btn')
 btn.addEventListener('click', () => {
     btn.style.backgroundColor = '#000000';
     btn.style.color = 'rgb(238, 241, 221)';
-    if(btn.innerText === "Add"){
+    if (btn.innerText === "Add") {
         btn.innerText = "Added";
     }
-    else{
+    else {
+        btn.style.backgroundColor = 'rgb(238, 241, 221)'
+        btn.style.color = '#000000';
         btn.innerText= "Add";
     }
 });
