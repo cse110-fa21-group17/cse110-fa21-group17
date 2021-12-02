333c1f788d718fcb6b23b8ccbac8accf29df0ffc
Raghav <rcherukuru@ucsd.edu>
2021-12-01 19:03:07 -0800

    Co-authored-by: benkwwuo <benkwwuo@users.noreply.github.com> Co-authored-by: Jackie Kwok <jkwok626@users.noreply.github.com> Co-authored-by: Eric Peng <3ricpeng@users.noreply.github.com> Co-authored-by: yingersun <yingersun@users.noreply.github.com>

--- app/public/javascripts/add_button.js
+++ app/public/javascripts/add_button.js
@@ -1,16 +1,19 @@
-let btn = document.getElementById('btn')
+let btn = document.querySelectorAll('#btn');
 
-btn.addEventListener('click', () => {
-    btn.style.backgroundColor = '#000000';
-    btn.style.color = 'rgb(238, 241, 221)';
-    if (btn.innerText === "Add") {
-        btn.innerText = "Added";
-    }
-    else {
-        btn.style.backgroundColor = 'rgb(238, 241, 221)'
-        btn.style.color = '#000000';
-        btn.innerText= "Add";
-    }
-});
+for(let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', () => {
+        if (btn[i].innerText === "Add") { //if the button is Add, change button to black, text to white, and text to Added
+            btn[i].innerText = "Added";
+            btn[i].style.backgroundColor = '#000000';
+            btn[i].style.color = 'rgb(238, 241, 221)';
+        }
+        else { //else button is Added, change button to white, text to black, and text to Add
+            btn[i].innerText= "Add";
+            btn[i].style.backgroundColor = 'rgb(238, 241, 221)';
+            btn[i].style.color = '#000000';
+        }
+    });
+}
+
 
 
