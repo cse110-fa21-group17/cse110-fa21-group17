9194cf21438de959cd310ba2cd96a88a9b8d3e41
Jackie Kwok <jackiekwok3@gmail.com>
2021-12-03 12:01:51 -0800

    Added bookmark (still need to connect to DB), Worked on Navbar responsiveness but still not working

---
+++ app/public/javascripts/landingPage.js
@@ -0,0 +1,30 @@
+window.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    // TODO: Replace with some kind of unique identifier for the bookmark in each card
+    bookmark = document.getElementById('someIdFromDatabase');
+
+    bookmark.addEventListener("click", () => {
+        if (bookmark.classList.contains("bi-bookmark")) {
+            bookmark.classList.replace("bi-bookmark", "bi-bookmark-fill");
+            console.log(bookmark);
+            // TODO: Mark this recipe as saved in database
+        } else if (bookmark.classList.contains("bi-bookmark-fill")) {
+            bookmark.classList.replace("bi-bookmark-fill", "bi-bookmark");
+            console.log(bookmark);
+            // TODO: Unsave this recipe in database
+        }
+    });
+}
+
+
+
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "nav-container") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav-container";
+  }
+}
