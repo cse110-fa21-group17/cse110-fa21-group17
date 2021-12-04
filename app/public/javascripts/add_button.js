7ebc36009b054fe549f068997b367e30d1c89405
Thomas Li <30820907+ThomasLiARDJAVA@users.noreply.github.com>
2021-12-04 01:11:35 +0000

    Calorie tracking (#60)
    * Co-authored-by: benkwwuo <benkwwuo@users.noreply.github.com>
    
    * fixed btn change from Added to Add
    
    * added citations for the timer implementation
    
    * Co-authored-by: benkwwuo <benkwwuo@users.noreply.github.com>
    Co-authored-by: Jackie Kwok <jkwok626@users.noreply.github.com>
    Co-authored-by: Eric Peng <3ricpeng@users.noreply.github.com>
    Co-authored-by: yingersun <yingersun@users.noreply.github.com>
    
    * added pie chart and recipe card
    
    * connected backend for calorie tracker
    
    * added calorie tracker link
    
    * fix: btn declaration
    
    * fixed top bar
    
    * added retrospective and sprint 2
    
    Co-authored-by: Raghav <rcherukuru@ucsd.edu>
    Co-authored-by: Benjamin Kuo <bkuo@ucsd.edu>

---
+++ app/public/javascripts/add_button.js
@@ -0,0 +1,19 @@
+let btn = document.querySelectorAll('.add_btn');
+
+for(let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', () => {
+        if (btn[i].innerText === "Add") { //if the button is Add, change button to black, text to white, and text to Added
+            btn[i].innerText = "Added";
+            btn[i].style.backgroundColor = '#000000';
+            btn[i].style.color = 'rgb(238, 241, 221)';
+        }
+        else { //else button is Added, change button to white, text to black, and text to Add
+            btn[i].innerText= "Add";
+            btn[i].style.backgroundColor = 'rgb(238, 241, 221)';
+            btn[i].style.color = '#000000';
+        }
+    });
+}
+
+
+
