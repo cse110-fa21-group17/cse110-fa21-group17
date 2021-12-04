80a9765f17e72c5cae2c2a4edc3935a65bac2990
Thomas Li <thomasligreat11@gmail.com>
2021-12-03 17:57:07 -0800

    removed init function for landing page

--- app/public/javascripts/landingPage.js
+++ app/public/javascripts/landingPage.js
@@ -1,26 +1,3 @@
-window.addEventListener('DOMContentLoaded', init);
-
-function init() {
-    // TODO: Replace with some kind of unique identifier for the bookmark in each card
-    // bookmark = document.querySelectorAll('#someIdFromDatabase');
-    // for (let i = 0; i < bookmark.length; i++) {
-    //   bookmark[i].addEventListener("click", () => {
-    //     if (bookmark[i].classList.contains("bi-bookmark")) {
-    //         bookmark[i].classList.replace("bi-bookmark", "bi-bookmark-fill");
-    //         console.log(bookmark);
-    //         // TODO: Mark this recipe as saved in database
-    //     } else if (bookmark[i].classList.contains("bi-bookmark-fill")) {
-    //         bookmark[i].classList.replace("bi-bookmark-fill", "bi-bookmark");
-    //         console.log(bookmark);
-    //         // TODO: Unsave this recipe in database
-    //     }
-    //   });
-    // }
-}
-
-
-
-
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "nav-container") {
