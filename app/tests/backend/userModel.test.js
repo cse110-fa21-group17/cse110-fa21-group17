dc649224c7a569235d6628000a0a8016436fc25b
Thomas Li <thomasligreat11@gmail.com>
2021-12-09 18:28:29 -0800

    added test for userModel

--- app/tests/backend/userModel.test.js
+++ app/tests/backend/userModel.test.js
@@ -4,12 +4,13 @@ const assert = require('assert');
 const userModel = require('../../database/models/usersModel');
 require('dotenv').config();
 
-describe('user model function tests', async function() {
+describe('user model function tests', function() {
     it('call getById with value null, expect error',
-        async function(done) {
-            const value = userModel.getById(null);
-            assert.equal(value, []);
-            console.log(value);
-            done();
+        function() {
+            return userModel.getById(null)
+                .then((value) => {
+                    assert.equal(value.length, 0);
+                })
         });
 });
+
